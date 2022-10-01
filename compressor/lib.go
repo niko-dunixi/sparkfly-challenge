22122c0f7f2e1dd88129e6e45071d45f7385fd33
Paul FREAKN Baker <paul-nelson-baker@users.noreply.github.com>
2022-10-01 01:20:40 -0500

    refactor: use io.Copy instead
    The standard library is much better and more efficient than I am
    in this method.

--- compressor/lib.go
+++ compressor/lib.go
@@ -12,20 +12,8 @@ func AsReader(input io.ReadCloser) io.Reader {
 		defer input.Close()
 		defer pipeWriter.Close()
 		defer gzipWriter.Close()
-
-		// TODO determine if io.Copy is actually a lazy operation
-		// or if it will blow our memory up
-		// _, _ = io.Copy(gzipWriter, input)
-
-		buffer := make([]byte, 1024)
-		for {
-			n, err := input.Read(buffer)
-			_, _ = gzipWriter.Write(buffer[:n])
-			if err == io.EOF {
-				return
-			} else if err != nil {
-				return
-			}
+		if _, err := io.Copy(gzipWriter, input); err != nil {
+			panic(err)
 		}
 	}()
 	return pipeReader
