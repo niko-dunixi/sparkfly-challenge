11b4782197f4f76f7119c2b8df0db7291d1c60f6
Paul FREAKN Baker <paul-nelson-baker@users.noreply.github.com>
2022-10-01 01:07:26 -0500

    refactor: extrapolating variable for redability

--- compressor/lib_test.go
+++ compressor/lib_test.go
@@ -11,12 +11,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func FuzzCompressor(f *testing.F) {
+func FuzzCompressorAsReader(f *testing.F) {
 	f.Add("Hi, I'm Paul")
 	f.Fuzz(func(t *testing.T, fuzzString string) {
+		// Simulate generic ReaderCloser
+		var input io.ReadCloser = ioutil.NopCloser(strings.NewReader(fuzzString))
 		// Convert this reader to a CompressionReader for downstream consumers
-		reader := AsReader(ioutil.NopCloser(strings.NewReader(fuzzString)))
-
+		reader := AsReader(input)
 		// Validate that the compression happened by decompressing and comparing with the input
 		actualOutput, err := io.ReadAll(reader)
 		if err != nil {
