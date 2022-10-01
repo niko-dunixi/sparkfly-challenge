c97b26fe0542f05f8171aef13c4c786325dc7835
Paul FREAKN Baker <paul-nelson-baker@users.noreply.github.com>
2022-10-01 01:09:18 -0500

    feat: large file tests
    - Includes generator and sha-sum for validation

--- compressor/lib_test.go
+++ compressor/lib_test.go
@@ -3,10 +3,16 @@ package compressor
 import (
 	"bytes"
 	"compress/gzip"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -29,3 +35,87 @@ func FuzzCompressorAsReader(f *testing.F) {
 		assert.Equal(t, []byte(fuzzString), actualBytes)
 	})
 }
+
+func TestCompressorAsReader(t *testing.T) {
+	t.Run("Random File", func(t *testing.T) {
+		sizesInMB := []int{1, 10, 20, 100, 1000}
+		for _, currentMB := range sizesInMB {
+			currentMB := currentMB
+			t.Run(fmt.Sprintf("%dmb", currentMB), func(t *testing.T) {
+				t.Parallel()
+				// Create our testable reader with an expected sha256 sum to validate
+				inputReader, expectedSha256Sum := generateRandomFile(t, 1024*1024*currentMB)
+				// Create our Compression Reader
+				reader := AsReader(inputReader)
+				// Unzip and hash the bytes, compare them against the generated hash
+				// to be sure we're doing things correctly
+				gzipReader, err := gzip.NewReader(reader)
+				if err != nil {
+					panic(err)
+				}
+				shaHasher := sha256.New()
+				if _, err := io.Copy(shaHasher, gzipReader); err != nil {
+					panic(err)
+				}
+				actualSha256Sum := hex.EncodeToString(shaHasher.Sum(nil))
+				assert.Equal(t, expectedSha256Sum, actualSha256Sum)
+			})
+		}
+	})
+}
+
+func generateRandomFile(t *testing.T, totalBytes int) (reader io.ReadCloser, expectedSha256Sum string) {
+	t.Helper()
+	// Create a new random file anc create a hasher. We will use a multi-writer
+	// to store the random bytes and generate a sum we can check in our test
+	random := rand.New(rand.NewSource(time.Now().Unix()))
+	file, err := os.CreateTemp("", fmt.Sprintf("*-random-%d.bin", totalBytes))
+	if err != nil {
+		panic(err)
+	}
+	shaHasher := sha256.New()
+	writer := io.MultiWriter(file, shaHasher)
+	func() {
+		// We create a nested function so we can be sure that the temporary file is
+		// closed when we've completed writing but before we return.
+		defer file.Close()
+		// Create our buffer and interate while we haven't hit our byte count
+		bytesWritten := 0
+		byteBuffer := make([]byte, 1024)
+		for bytesWritten < totalBytes {
+			// Create an intermedate buffer that is dynamically sized so we can write exactly
+			// the number of bytes we want.
+			intermediateBuffer := byteBuffer[:min(len(byteBuffer), totalBytes-bytesWritten)]
+			_, _ = random.Read(intermediateBuffer)
+			n, err := writer.Write(intermediateBuffer)
+			if err != nil {
+				panic(err)
+			}
+			bytesWritten += n
+		}
+	}()
+	// Return the file for reading with the sum for validation in the test
+	fileReader, err := os.Open(file.Name())
+	if err != nil {
+		panic(err)
+	}
+	return fileReader, hex.EncodeToString(shaHasher.Sum(nil))
+}
+
+type Int interface {
+	byte | int | int32 | int64 | uint | uint32 | uint64
+}
+
+func min[T Int](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// func RandomBytes(total int) (input io.ReadCloser, expected io.Reader) {
+// 	random := rand.New(rand.NewSource(time.Now().Unix()))
+// 	pipeReader, pipeWriter := io.Pipe()
+// 	teeReader := io.TeeReader(random, pipeWriter)
+// 	return ioutil.NopCloser(teeReader), pipeReader
+// }
